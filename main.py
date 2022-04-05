a1d108715c3a68fafafde632e429522c346e588b
radturkin <67508317+radturkin@users.noreply.github.com>
2022-04-05 12:01:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
-x_values=[]
-y_values=[]
-corner_points=[]
+
 @app.route('/coordinates', methods=['POST'])
 def findcoords():
+    
+    x_values=[]
+    y_values=[]
+    corner_points=[]
+    
+    #take in values from form
     h = int(request.form['height'])
     w = int(request.form['width'])
     corner_points = list(eval(request.form['corner_points']))
-
-   # corner_points = request.form['corner_points']
-
+    
+    #rearrange/tidyup corner points 
     for i,j in corner_points:
         x_values.append(i)
         y_values.append(j)
@@ -26,6 +29,8 @@ def findcoords():
     y=np.array(y_values)
     x=np.unique(x)
     y=np.unique(y)
+    
+    #calculate and organize all coordinate pairs for pixel placement
     xs=np.linspace(x[0],x[1],w)
     ys=np.linspace(y[0],y[1],h)
     Xs,Ys = np.meshgrid(xs,ys)
@@ -34,13 +39,8 @@ def findcoords():
     coordinates=np.flip(xys,0)
 
     return render_template("coordinates.html", coordinates=coordinates)
-    #if not height or not width or not corner_points:
-     #   return render_template('index.html')
-#@app.route('/solution', method=['POST'])
-#def corner_sorter(h,w,corner_points):
-  
 
-  #return render_template("coordinates.html)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=port)
