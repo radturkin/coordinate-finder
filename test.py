48a8f267bcb104e4415d93dcbc79fbeff0806ed6
radturkin <67508317+radturkin@users.noreply.github.com>
2022-04-06 08:43:16 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
+import unittest
 
+from grpc import StatusCode
+
+from main import app
+
+class flask_test(unittest.TestCase):
+
+    def test1(self):
+        pass
+
+    def test_index(self):
+        tester=app.test_client(self)
+        response=tester.get("/")
+        statuscode=response.status_code
+        self.assertEqual(statuscode,200)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
